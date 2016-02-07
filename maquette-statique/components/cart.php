faf2f9828036a91bbb6f6ac16131f98182446b88
Stéhane Lam <stelam@gmail.com>
2016-02-07 13:29:10 -0500

    ajout checkout steps

--- maquette-statique/components/cart.php
+++ maquette-statique/components/cart.php
@@ -1,1 +1,19 @@
-cart
+<div class="row upperNavigationContainer">
+	<ul class="upperNavigation">	
+		<li title="1" class="active externalNavIcon">
+			<a data-guide-toggle="tab">
+				<span>Récapitulatif</span>
+			</a>			
+		</li>	
+		<li title="2" class="externalNavIcon">
+			<a data-guide-toggle="tab">
+				<span>Identification</span>
+			</a>			
+		</li>	
+		<li title="3" class="warn-error externalNavIcon">
+			<a data-guide-toggle="tab">
+				<span>Paiement</span>
+			</a> 
+		</li>
+	</ul>
+</div>

a2b12c9e4e86ceff551da08b0d5c43bcebbc6d8b
Stéhane Lam <stelam@gmail.com>
2016-02-07 21:50:49 -0500

    completion des checkout steps

--- maquette-statique/components/cart.php
+++ maquette-statique/components/cart.php
@@ -1,19 +1,26 @@
+<header>
+	<h1><span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span> Passer à la caisse</h1>
+</header>
+
 <div class="row upperNavigationContainer">
 	<ul class="upperNavigation">	
-		<li title="1" class="active externalNavIcon">
-			<a data-guide-toggle="tab">
-				<span>Récapitulatif</span>
-			</a>			
+		<li title="1" class="<?= ($_GET['step'] == 'review') ? 'active' : '' ?> externalNavIcon">
+			<a data-guide-toggle="tab" href="?page=panier&step=review">
+				<span>Revue des items <span class="glyphicon glyphicon-ok" aria-hidden="true"></span></span>
+			</a>
+			<span class="step-nb">1</span>			
 		</li>	
-		<li title="2" class="externalNavIcon">
-			<a data-guide-toggle="tab">
-				<span>Identification</span>
-			</a>			
+		<li title="2" class="<?= ($_GET['step'] == 'identification') ? 'active' : '' ?> externalNavIcon">
+			<a data-guide-toggle="tab" href="?page=panier&step=identification">
+				<span>Identification <span class="glyphicon glyphicon-ok" aria-hidden="true"></span></span>
+			</a>	
+			<span class="step-nb">2</span>		
 		</li>	
-		<li title="3" class="warn-error externalNavIcon">
-			<a data-guide-toggle="tab">
-				<span>Paiement</span>
+		<li title="3" class="<?= ($_GET['step'] == 'payment') ? 'active' : '' ?> externalNavIcon" href="?page=panier&step=payment">
+			<a data-guide-toggle="tab" href="?page=panier&step=payment">
+				<span>Paiement et confirmation <span class="glyphicon glyphicon-ok" aria-hidden="true"></span></span>
 			</a> 
+			<span class="step-nb">3</span>
 		</li>
 	</ul>
 </div>

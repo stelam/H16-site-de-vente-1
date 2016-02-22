39d288a01a2f664cebca2e55f9a08b8de917c090
Stéphane Lam <stephane.lam.1@ens.etsmtl.ca>
2016-02-21 20:43:32 -0500

    debut du routing des pages

---
+++ app/js/angular-routes.js
@@ -0,0 +1,15 @@
+(function(){   
+ "use strict";
+
+  angular.module('app')
+    .config(['$routeProvider', function($routeProvider){
+	    $routeProvider.
+	      when('/featuring', {
+	        templateUrl: 'partials/featuring-list.html',
+	        controller: 'showListController'
+	      }).
+	      otherwise({
+	        redirectTo: '/featuring'
+	      });
+    }])
+})();

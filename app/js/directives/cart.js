077ebc816adb6b3dd8dc8fc007ed0f2faf1500ce
Stéphane Lam <stephane.lam.1@ens.etsmtl.ca>
2016-02-24 16:58:58 -0500

    popover sur le widget panier

--- app/js/directives/cart.js
+++ app/js/directives/cart.js
@@ -5,7 +5,7 @@
     "use strict";
 
     angular.module('app')
-        .directive('cart', ['cartService', 'DIRECTIVE_TEMPLATE_PATH', function (cartService, DIRECTIVE_TEMPLATE_PATH) {
+        .directive('cart', ['cartService', 'DIRECTIVE_TEMPLATE_PATH', '$timeout', function (cartService, DIRECTIVE_TEMPLATE_PATH, $timeout) {
             return {
                 restrict: 'E',
                 scope: {
@@ -14,6 +14,30 @@
                 templateUrl: DIRECTIVE_TEMPLATE_PATH+"/cart-template.html",
                 link: function (scope, element) {
                     scope.currentCart = cartService.currentCart;
+                    scope.initialized = false;
+
+                    // écouter les changements faits au panier
+                    scope.$watch('currentCart', function(newCart){
+                        if (scope.initialized)
+                            scope.flashCart();
+                        else {
+                            scope.initialized = true;
+                        }
+                    }, true);
+
+
+                    $(element).find("#cart-btn").popover({
+                        html: true,
+                        trigger : 'manual',
+                        placement : 'bottom'
+                    });
+
+                    scope.flashCart = function(){
+                        $(element).find(".nb-cart-items").removeClass("animate");
+                        $(element).find("#cart-btn").popover('show');
+                        $(element).find(".nb-cart-items").addClass("animate");
+                    }
+
                 }
             }
         }]);
--- app/js/static-scripts.js
+++ app/js/static-scripts.js
@@ -1,67 +1,20 @@
 // SCRIPTS TEMPORAIRES POUR LA MAQUETTE STATIQUE
 
-$(window).scroll(function() {
-	if ($(this).scrollTop() > 1){  
-		$('.site-nav').addClass("sticky");
-		$('#admin .footer').css("position","");
-		$('#admin .footer').css("bottom","");
-	}else{
-		$('.site-nav').removeClass("sticky");
-		$('#admin .footer').css("position","absolute");
-		$('#admin .footer').css("bottom","0");
-	}
-});
 
-
-$(function(){
-  var hash = window.location.hash;
-  hash && $('ul.nav a[href="' + hash + '"]').tab('show');
-
-  $('.navbar-nav a, .titleShow').click(function (e) {
-    $(this).tab('show');
-    var scrollmem = $('body').scrollTop();
-    window.location.hash = this.hash;
-    $('html,body').scrollTop(scrollmem);
-  });
-});
 
 $(document).ready(function(){
+
 	$('html').click(function(e) {
 		if (!$(e.target).hasClass('popover') && $(e.target).closest('.popover').length < 1){
 	    	$('.popover').popover('hide');
 	    }
 	});
 
-	$(function () {
-		$('[data-toggle="popover"]').popover({
-			html: true,
-			trigger : 'manual',
-			placement : 'bottom'
-		})
-	})
-
-	$(".btn-add").click(function(e){
-		$('.nb-cart-items').removeClass("animate");
-		$("#cart-btn").popover('show');
-		$('.nb-cart-items').text(parseInt($('.nb-cart-items').text()) + 1).addClass('animate');
-	    e.stopPropagation();
-	})
-
 	$("#search-toggle-btn").click(function(){
 		$(".search-wrapper").toggleClass("on");
 		$("#search").focus();
 	})
 
-    $(function () {
-        $('#calendar').datetimepicker({useCurrent: false, debug: false});
-
-        $('#calendar').on('dp.change', function(e){
-        	var date = moment(e.date._d);
-        	moment.locale("fr-CA");
-        	var dateString = moment(date).format('DD/MM/YYYY');
-        	window.location.href = 'spectacles-date.html'
-        })
-    });
 
 })
 

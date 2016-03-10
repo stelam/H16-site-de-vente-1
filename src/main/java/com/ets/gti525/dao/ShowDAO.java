7ac089ffb7d093193eb7c0928a0b24fcc82a5c78
Stéphane Lam <stephane.lam.1@ens.etsmtl.ca>
2016-03-10 02:19:58 -0500

    much sorry, many commits

--- src/main/java/com/ets/gti525/dao/ShowDAO.java
+++ src/main/java/com/ets/gti525/dao/ShowDAO.java
@@ -2,13 +2,13 @@ package com.ets.gti525.dao;
 
 import com.ets.gti525.model.Show;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface ShowDAO extends JpaRepository<Show, Long> {
+public interface ShowDAO extends CrudRepository<Show, Long>, JpaSpecificationExecutor<Show> {
 
     Show findShowByName(String name);
     List<Show> findByArtistName(String artistName);

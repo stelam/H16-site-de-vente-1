1aa972d8fdd277201aa88e3684b216673270a25b
Stéphane Lam <stephane.lam.1@ens.etsmtl.ca>
2016-04-07 13:02:23 -0400

    search unique fix

--- app/js/app.js
+++ app/js/app.js
@@ -38,6 +38,19 @@
 		$httpProvider.defaults.withCredentials = true;
 	}])
 
+	angular.module('app').filter('unique', function() {
+
+	  return function (arr, field) {
+	    var o = {}, i, l = arr.length, r = [];
+	    for(i=0; i<l;i+=1) {
+	      o[arr[i][field]] = arr[i];
+	    }
+	    for(i in o) {
+	      r.push(o[i]);
+	    }
+	    return r;
+	  };
+	})
 
 })();
 
